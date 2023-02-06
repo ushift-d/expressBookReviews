2b37a35b5062036446a7cfd787214ac402642c28
Owner <ushift@DESKTOP-HHULJ21.localdomain>
2023-02-06 10:10:32 -0600

    added authentication and registered users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -19,21 +19,50 @@ const isValid = (username)=>{ //returns boolean
   }
 }
 
+// change this
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  const authuser = users.filter((user) =>{
+    return (user.username === username && user.password === password)
+  });
+  if (authuser.length > 0) {
+    return true;
+  } else {
+    return false;
+  } 
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn + "";
+  const review = req.query.review;
+  const username = req.user.data;
+  const book = books[isbn];
+  if (book) {
+        book.reviews[username] = review;
+        return res.status(200).json(book);
+    }
+    return res.status(404).json({ message: "Invalid ISBN" });
 });
+  
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let filtered_book = books[isbn]
+  if (filtered_book) {
+      let review = req.query.review;
+      let reviewer = req.session.authorization['username'];
+      if(review) {
+          filtered_book['reviews'][reviewer] = review;
+          books[isbn] = filtered_book;
+      }
+      res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+  }
+  else{
+      res.send("Unable to find this ISBN!");
+  }
 });
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
